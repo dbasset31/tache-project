d3928ab82274eb251de84817170366aecae2d26c
donovan <admin@sanaria.tk>
2022-07-04 04:15:19 +0200

    correction route

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::middleware('auth')->group(function() {
-
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::middleware('guest')->group(function() {
     Route::get('/connexion', function () {
